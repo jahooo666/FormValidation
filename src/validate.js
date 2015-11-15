bab6c342dce0c7eb498db1867fe4336a5cbab083
jahooo666 <okraskaj@ee.pw.edu.pl>
2015-11-15 23:45:33 +0100

    Initial commit

---
+++ src/validate.js
@@ -0,0 +1,170 @@
+function isEmpty(element){
+    if(element.value!=''){
+        element.style.backgroundColor = "#FFFFFF"
+        return false;
+    }else{
+        //alert("To pole musi być wypełnione!");
+        element.style.backgroundColor = "#E52B50";
+        element.onkeypress = function(){
+            isEmpty(element);
+        };
+        return true;
+    }
+}
+
+function checkPasswd(element) {
+    var str = element.value;
+    if (str.length < 6) {
+        alert("haslo jest za krótkie");
+        return ("too short");
+    } else if (str.length > 50) {
+        alert("hasło jest za długie");
+        return ("too long");
+    } else if (str.search(/\d/) == -1) {
+        alert("W haśle powinny być liczby");
+        return ("no num");
+    } else if (str.search(/[a-zA-Z]/) == -1) {
+        alert("W haśle powinny być litery");
+        return ("no letter");
+    } else if (str.search(/[^a-zA-Z0-9\!\@\#\$\%\^\&\*\(\)\_\+\.\,\;\:]/) != -1) {
+        alert("Niedozwolony znak");
+        return ("bad char");
+    }
+    alert("Hasło ok!!");
+    return ("ok");
+}
+
+function setSex(plec){
+    if(plec=="kobieta") {
+        woman.setAttribute("checked", "checked");
+        man.removeAttribute("checked");
+    }else if(plec == "mezczyzna"){
+        man.setAttribute("checked","checked");
+        woman.removeAttribute("checked");
+    }
+}
+
+
+function isWoman(element){
+    //words[] = element.value.s
+    var splited = element.value.split(" ");
+    //alert(imie);
+    if (splited.length == 2){
+        imie = splited[0];
+        if(imie[imie.length-1]=="a"){
+            setSex("kobieta");
+        }else{
+            setSex("mezczyzna");
+        }
+    }
+}
+
+function isWomanFromPesel(element){
+    var znak = element.value.charAt(element.value.length-2);
+    if(znak%2!=0){
+        setSex("mezczyzna");
+    }else{
+        setSex("kobieta");
+    }
+}
+
+function setErrorText(element, text){
+    parent = element.parentElement;
+    parent.innerHTML
+}
+
+function getDateFromPesel(element){
+    var year = 1900 + parseInt(element.value.substr(0,2));
+    var month = element.value.substr(2,2);
+    var day = element.value.substr(4,2);
+    var fulldate = year + "-" + month + "-" + day;
+    birthdate.value = fulldate;
+    //alert(year + "-" + month + "-" + day);
+    //TODO: zrobić
+}
+
+function isComplicated(element){
+    if(element.value.length<7){
+        //	alert("Za krótkie haslo!");
+    }
+}
+
+function arePasswdsTheSame(element, element2){
+    if(element.value != element2.value){
+        alert("hasla są różne!");
+    }
+}
+
+function isLongEnoughtPesel(element){
+    if(element.value.length != 11){}
+    //	alert("zla dlugosc peselu");
+}
+
+var fullname = document.getElementById('fullname');
+var login = document.getElementById('login');
+var passwd = document.getElementById('password');
+var passwdrp = document.getElementById('password-rep');
+//var sex = document.getElementById('sex');
+
+var woman = document.getElementsByName('sex2')[0];
+var man = document.getElementsByName('sex1')[0];
+
+
+var pesel = document.getElementById('pesel');
+var birthdate = document.getElementById('date');
+var photo = document.getElementById('photo');
+var form = document.getElementById('form');
+var sexBox = document.getElementById('sexBox');
+
+
+fullname.onblur = function(){
+    isEmpty(this);
+    if(this.value == "Maciej Kłos"){
+        var pedal = document.createElement("input");
+        pedal.setAttribute("id","pedal");
+        pedal.setAttribute("type","radio");
+        sexBox.appendChild(pedal);
+        sex1= document.getElementsByName('sex1')[0];
+        sex2= document.getElementsByName('sex2')[0];
+        sex1.checked = false;
+        sex1.disabled = true;
+        sex2.disabled = true;
+        pedal.setAttribute("checked","checked");
+        //sex1.setAttribute("checked","false");
+        var nazwaPedal = document.createElement("p");
+        sexBox.innerHTML += "pedal";
+        sexBox.appendChild(nazwaPedal);
+    }
+    isWoman(this);
+};
+
+login.onblur = function(){
+    isEmpty(this);
+};
+
+passwd.onblur = function(){
+    isEmpty(this);
+    checkPasswd(this);
+};
+
+passwdrp.onblur = function(){
+    isEmpty(this);
+    arePasswdsTheSame(this,passwd);
+};
+
+pesel.onblur = function(){
+    isEmpty(this);
+    isLongEnoughtPesel(this);
+    isWomanFromPesel(this);
+    getDateFromPesel(this);
+};
+
+birthdate.onblur = function(){
+    isEmpty(this);
+};
+
+photo.onblur = function(){
+    isEmpty(this);
+};
+
+
